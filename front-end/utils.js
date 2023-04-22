7ecba55f3b64da14d4afabe078f05d20e1e8b426
carlos Rodrigues <>
2023-04-21 21:49:48 -0300

    refactor: Cleaning utils.js

--- front-end/utils.js
+++ front-end/utils.js
@@ -13,58 +13,66 @@ function setPrice(increment) {
   const priceInput = document.getElementById("price");
   priceInput.value = (parseFloat(priceInput.value) + increment).toFixed(2);
 }
-
-function revisionOfValues() {
+function validateOfValues() {
   const plate = document.getElementById("plate").value;
   const price = parseFloat(document.getElementById("price").value);
-
-  const dateStr = document.getElementById("date").value;
-  const year = dateStr.split("-")[0];
+  const date = document.getElementById("date").value;
 
   const plateValid = validatePlate(plate);
+  const dateValid = validateDate(date);
   const priceValid = validatePrice(price);
 
-  if (!plateValid.valid) {
-    showError("plate", plateValid.message);
-    animateInvalidPrice(document.getElementById("plate"));
-    document.getElementById("plate").focus();
-  } else if (date === "" || year < 2023) {
-    showError(
-      "date",
-      "A data deve conter apenas números e um ano maior que 2023."
-    );
-    animateInvalidPrice(document.getElementById("date"));
-    document.getElementById("date").focus();
-  } else if (!priceValid.valid) {
-    showError("price", priceValid.message);
-    animateInvalidPrice(document.getElementById("price"));
-    document.getElementById("price").focus();
+  const listErrors = [
+    { field: "plate", message: "A placa deve ter 7 caracteres." },
+    {
+      field: "date",
+      message: "A data deve conter apenas números e um ano maior que 2023.",
+    },
+    {
+      field: "price",
+      message: "O preço deve estar entre R$0,01 e R$1.000,00.",
+    },
+  ];
+
+  if (!plateValid) {
+    showError(listErrors[0].field, listErrors[0].message);
+  } else if (!dateValid) {
+    showError(listErrors[1].field, listErrors[1].message);
+  } else if (!priceValid) {
+    showError(listErrors[2].field, listErrors[2].message);
   } else {
     request();
   }
 }
 
 function validatePlate(plate) {
-  plate = plate.replace("  " && " ", "");
+  plate = plate.replace(/ /g, "");
   if (plate.length !== 7) {
-    return { valid: false, message: "A placa deve ter 7 caracteres." };
+    return false;
   }
-  return { valid: true };
+  return true;
+}
+
+function validateDate(date) {
+  const year = date.split("-")[0];
+  console.log(year);
+  if (date === "" || year < 2023) {
+    return false;
+  }
+  return true;
 }
 
 function validatePrice(price) {
-  if (price <= 0 || price > 1000) {
-    return {
-      valid: false,
-      message: "O preço deve estar entre R$0,01 e R$1.000,00.",
-    };
+  if (price < 0.01 || price > 1000) {
+    return false;
   }
-  return { valid: true };
+  return true;
 }
 
 function showError(elementId, message) {
   const element = document.getElementById(elementId);
   element.classList.add("invalid");
+  animateInvalidPrice(element);
   alert(message);
 }
 
@@ -101,16 +109,13 @@ function request() {
     service_description: description,
   };
 
-  fetch(
-    "http://localhost:8000/api/services/",
-    {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(data),
-    }
-  )
+  fetch("http://localhost:8000/api/services/", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  })
     .then((response) => response.json())
     .then((data) => {
       console.log("Success:", data);

9af0f6a50e0bed96fc6bdcf69545e9f69b36c4f5
Ricardo Maisonnave <4395602+Rioos@users.noreply.github.com>
2023-04-26 01:41:21 -0300

    Update utils.js

--- front-end/utils.js
+++ front-end/utils.js
@@ -1,3 +1,15 @@
+  const ERRORS = {
+    "INVALID_DATE": {
+      field: "date",
+      message: "A data deve conter apenas números e um ano maior que 2023.",
+    },
+    "INVALID_PLATE": { field: "plate", message: "A placa deve ter 7 caracteres." },
+    "INVALID_PRICE": {
+      field: "price",
+      message: "O preço deve estar entre R$0,01 e R$1.000,00.",
+    }
+  }
+
 function setDateInputValue(date) {
   const today = new Date();
   const dd = String(today.getDate()).padStart(2, "0");
@@ -13,70 +25,45 @@ function setPrice(increment) {
   const priceInput = document.getElementById("price");
   priceInput.value = (parseFloat(priceInput.value) + increment).toFixed(2);
 }
+
 function validateOfValues() {
   const plate = document.getElementById("plate").value;
   const price = parseFloat(document.getElementById("price").value);
   const date = document.getElementById("date").value;
 
-  const plateValid = validatePlate(plate);
-  const dateValid = validateDate(date);
-  const priceValid = validatePrice(price);
-
-  const listErrors = [
-    { field: "plate", message: "A placa deve ter 7 caracteres." },
-    {
-      field: "date",
-      message: "A data deve conter apenas números e um ano maior que 2023.",
-    },
-    {
-      field: "price",
-      message: "O preço deve estar entre R$0,01 e R$1.000,00.",
-    },
-  ];
-
-  if (!plateValid) {
-    showError(listErrors[0].field, listErrors[0].message);
-  } else if (!dateValid) {
-    showError(listErrors[1].field, listErrors[1].message);
-  } else if (!priceValid) {
-    showError(listErrors[2].field, listErrors[2].message);
-  } else {
-    request();
+  try {
+    const plateValid = validatePlate(plate)
+    const dateValid = validateDate(date)
+    const priceValid = validatePrice(price)
+    const response = request()
+  } catch (err) {
+    showError(ERRORS[err.message])
   }
+ 
 }
 
 function validatePlate(plate) {
   plate = plate.replace(/ /g, "");
-  if (plate.length !== 7) {
-    return false;
-  }
-  return true;
+  if (plate.length !== 7) throw new Error("INVALID_DATE")
 }
 
 function validateDate(date) {
   const year = date.split("-")[0];
-  console.log(year);
-  if (date === "" || year < 2023) {
-    return false;
-  }
-  return true;
+  if (date === "" || year < 2023) throw new Error("INVALID_DATE")
 }
 
 function validatePrice(price) {
-  if (price < 0.01 || price > 1000) {
-    return false;
-  }
-  return true;
+  if (price < 0.01 || price > 1000) throw new Error("INVALID_PRICE")
 }
 
-function showError(elementId, message) {
-  const element = document.getElementById(elementId);
+function showError({field, message}) {
+  const element = document.getElementById(field);
   element.classList.add("invalid");
-  animateInvalidPrice(element);
+  animateError(element);
   alert(message);
 }
 
-function animateInvalidPrice(element) {
+function animateError(element) {
   element.animate(
     [
       { transform: "translateX(-10px)" },
@@ -96,31 +83,26 @@ function animateInvalidPrice(element) {
   );
 }
 
-function request() {
-  var plate = document.getElementById("plate").value;
-  var date = document.getElementById("date").value;
-  var price = document.getElementById("price").value;
-  var description = document.getElementById("description").value;
+async function request({plate, date, price}) {
+  const description = document.getElementById("description").value;
 
-  var data = {
+  const data = {
     plate: plate,
     price: price,
     service_date: date,
     service_description: description,
   };
-
-  fetch("http://localhost:8000/api/services/", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(data),
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      console.log("Success:", data);
+  
+  try {
+    const response = await fetch("http://localhost:8000/api/services/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
     })
-    .catch((error) => {
-      console.error("Error:", error);
-    });
+    console.log("Success:", response.json());
+  } catch (error) {
+    console.error("Error:", error);
+  }
 }

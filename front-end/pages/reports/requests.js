7b467c5dd1a21e0c8ade7584cd581f977fe9d74d
carlos Rodrigues <>
2023-05-24 14:26:16 -0300

    refactor: Clean reports class

--- front-end/pages/reports/requests.js
+++ front-end/pages/reports/requests.js
@@ -1,18 +1,20 @@
 const ip = getIp();
 
-function reportDate() {
+function reportByDate() {
   const index = document.getElementById("reporttype").value;
   const date = document.getElementById("date").value;
-  try {
-    if (index != 3) validateDate(date);
-  } catch (err) {
-    throw err;
+  if (index != 3) {
+    try {
+      validateDate(date);
+    } catch (err) {
+      throw err;
+    }
   }
 
-  return concatDate(date, index);
+  return formatDateParameter(date, index);
 }
 
-function concatDate(date, index) {
+function formatDateParameter(date, index) {
   const [year, month, day] = date.split("-");
   return {
     0: `day=${day}&month=${month}&year=${year}`,
@@ -22,74 +24,96 @@ function concatDate(date, index) {
   }[index];
 }
 
-function typeReport() {
-  const plateField = document.querySelector("#plate-field");
-
-  if (plateField.style.display !== "none") {
-    return reportPlate();
-  } else {
-    return reportDate();
-  }
+function identifyReportType() {
+  return isPlateFieldDisplayed() ? reportByPlate() : reportByDate();
 }
 
-function reportPlate() {
+function isPlateFieldDisplayed() {
+  return document.querySelector("#plate-field").style.display !== "none";
+}
+
+function reportByPlate() {
   const plate = document.getElementById("plate").value;
   validatePlate(plate);
   return `plate_number=${plate}`;
 }
 
-async function request() {
+async function requestServiceReports() {
   try {
-    const services = await getServiceReports(typeReport());
+    const services = await getServiceReports(identifyReportType());
     validateResponse(services);
     services.date = filterDate(services);
-    createTable(services);
+    populateTableWithServices(services);
   } catch (err) {
     showError(err);
   }
 }
 
-function createTable(services) {
+function populateTableWithServices(services) {
+  const tableBody = document.getElementById("table-body");
+  clearTable();
+  services.forEach((service) => {
+    addServiceToTable(service);
+  });
+
+  addTotalRow(services, tableBody);
+}
+
+function clearTable() {
   const tableBody = document.getElementById("table-body");
   tableBody.innerHTML = "";
   document.querySelector("table").style.display = "block";
-
-  services.forEach((service) => {
-    const {
-      id,
-      license_plate: { number },
-      type: { name },
-      price,
-      date,
-      description,
-    } = service;
-    const row = tableBody.insertRow();
-
-    row.insertCell().textContent = number;
-    row.insertCell().textContent = name;
-    row.insertCell().textContent = price;
-    row.insertCell().textContent = date.split("-").reverse().join("-");
-    row.insertCell().textContent = description;
-
-    createEditButton(row, { id, number, name, price, date, description });
-    createDeleteButton(row, id);
-  });
-
-  rowReport(services, tableBody);
 }
 
-function createDeleteButton(row, id) {
-  const deleteCell = row.insertCell();
-  const deleteButton = document.createElement("a");
-  deleteButton.innerHTML =
-    "<i class='fa fa-trash' alt='Excluir' style='font-size:36px'></i>";
-  deleteButton.classList.add("delete-button");
+function addServiceToTable(service) {
+  const tableBody = document.getElementById("table-body");
+  const row = tableBody.insertRow();
+  const {
+    id,
+    license_plate: { number },
+    type: { name },
+    price,
+    date,
+    description,
+  } = service;
 
-  deleteButton.addEventListener("click", () => {
-    confirmDelete(id);
-  });
+  row.insertCell().textContent = number;
+  row.insertCell().textContent = name;
+  row.insertCell().textContent = price;
+  row.insertCell().textContent = date;
+  row.insertCell().textContent = description;
 
-  deleteCell.appendChild(deleteButton);
+  addButtonToTableRow(
+    row,
+    { id, number, name, price, date, description },
+    createEditButton,
+    "edit-button"
+  );
+  addButtonToTableRow(row, id, createDeleteButton, "delete-button");
+}
+
+function addButtonToTableRow(row, data, buttonCreationFunction, buttonClass) {
+  const cell = row.insertCell();
+  const button = buttonCreationFunction(data);
+
+  button.classList.add(buttonClass);
+  cell.appendChild(button);
+}
+
+function createDeleteButton(id) {
+  const deleteButton = createIconButton(
+    "fa fa-trash",
+    "Excluir",
+    "font-size:36px"
+  );
+  deleteButton.addEventListener("click", () => confirmDelete(id));
+  return deleteButton;
+}
+
+function createIconButton(iconClass, altText, style) {
+  const iconButton = document.createElement("a");
+  iconButton.innerHTML = `<i class='${iconClass}' alt='${altText}' style='${style}'></i>`;
+  return iconButton;
 }
 
 function confirmDelete(id) {
@@ -105,43 +129,38 @@ function confirmDelete(id) {
       deleteService(id);
     }
   });
-  reload();
 }
 
-function createEditButton(row, rowData) {
-  const editCell = row.insertCell();
-  const editButton = document.createElement("a");
-  editButton.innerHTML =
-    "<i class='fa fa-edit' alt='Editar' style='font-size:36px'></i>";
-  editButton.classList.add("edit-button");
-
-  editButton.addEventListener("click", () => {
-    modalUpdate(rowData);
-  });
-
-  editCell.appendChild(editButton);
+function reload() {
+  document.getElementById("submit").click();
 }
 
-async function modalUpdate(rowData) {
+function createEditButton(rowData) {
+  const editButton = createIconButton("fa fa-edit", "Editar", "font-size:36px");
+  editButton.addEventListener("click", () => openModalToUpdate(rowData));
+  return editButton;
+}
+
+async function openModalToUpdate(rowData) {
   Swal.fire({
     title: "Editar serviço",
     html: `
-      <div class="form-group">
-        <h4>id do serviço:${rowData.id}</h4>
-        <label for="plate">Placa do veículo</label>
-        <input type="text" id="plate-form" name="plate" maxlength="7" value=${rowData.number}>
-        <label for="plate">Serviço realizado</label>
-        <select id="report-type">
-          <option value="error">Selecione o serviço</option>
-        </select>
-        <label for="date">Data do serviço</label>
-        <input type="date" id="date-form" name="date" value=${rowData.date} />
-        <label for="price">Preço do serviço</label>
-        <input type="number" name="price" id="price" value=${rowData.price} />
-        <label for="description">Descrição:</label>
-        <textarea type="text" id="description">${rowData.description}</textarea>
-      </div>
-    `,
+        <div class="form-group">
+            <h4>id do serviço:${rowData.id}</h4>
+            <label for="plate">Placa do veículo</label>
+            <input type="text" id="plate-form" name="plate" maxlength="7" value=${rowData.number}>
+            <label for="plate">Serviço realizado</label>
+            <select id="report-type">
+            <option value="error">Selecione o serviço</option>
+            </select>
+            <label for="date">Data do serviço</label>
+            <input type="date" id="date-form" name="date" value=${rowData.date} />
+            <label for="price">Preço do serviço</label>
+            <input type="number" name="price" id="price" value=${rowData.price} />
+            <label for="description">Descrição:</label>
+            <textarea type="text" id="description">${rowData.description}</textarea>
+        </div>
+          `,
     showCancelButton: true,
     confirmButtonText: "Confirmar",
     cancelButtonText: "Cancelar",
@@ -150,6 +169,7 @@ async function modalUpdate(rowData) {
       try {
         validateOfValues(fieldsUpdate());
         updateService({ id, ...fieldsUpdate() });
+        reload();
       } catch (err) {
         const errorMessage = err.message;
         Swal.showValidationMessage(ERRORS[errorMessage].message);
@@ -157,38 +177,6 @@ async function modalUpdate(rowData) {
     },
   });
   await responseTypes(rowData.name);
-}
-
-function fieldsUpdate() {
-  const plate = document.getElementById("plate-form").value;
-  const type = document.getElementById("report-type").value;
-  const date = document.getElementById("date-form").value;
-  const price = document.getElementById("price").value;
-  return { plate, type, date, price };
-}
-
-function reload() {
-  document.querySelector("#submit").click();
-}
-
-function rowReport(services, tableBody) {
-  const size = services.length;
-  const totalPrice = filterPrice(services);
-  const totalRow = tableBody.insertRow();
-  const totalCell = totalRow.insertCell();
-  totalCell.colSpan = "2";
-  totalCell.textContent = `Total de serviços: ${size}`;
-  totalRow.insertCell().textContent = `Dinheiro arrecadado: R$${totalPrice.toFixed(
-    2
-  )}`;
-}
-
-function filterDate(services) {
-  return services.sort(({ date: a }, { date: b }) => new Date(a) - new Date(b));
-}
-
-function filterPrice(services) {
-  return services.reduce((acc, { price }) => acc + parseFloat(price), 0);
 }
 
 function radioReport() {
@@ -206,4 +194,31 @@ function radioReport() {
   plateField.style.display = radio !== "date" ? "flex" : "none";
   plateInput.value = radio === "date" ? "" : "";
   dateInput.value = radio !== "date" ? "" : "";
+}
+
+function filterDate(services) {
+  return services.sort(({ date: a }, { date: b }) => new Date(a) - new Date(b));
+}
+
+function filterPrice(services) {
+  return services.reduce((acc, { price }) => acc + parseFloat(price), 0);
+}
+
+function addTotalRow(services, tableBody) {
+  const size = services.length;
+  const totalPrice = filterPrice(services);
+  const totalRow = tableBody.insertRow();
+  const totalCell = totalRow.insertCell();
+  totalCell.colSpan = "2";
+  totalCell.textContent = `Total de serviços: ${size}`;
+  totalRow.insertCell().textContent = `Dinheiro arrecadado: R$${totalPrice.toFixed(
+    2
+  )}`;
+}
+function fieldsUpdate() {
+  const plate = document.getElementById("plate-form").value;
+  const type = document.getElementById("report-type").value;
+  const date = document.getElementById("date-form").value;
+  const price = document.getElementById("price").value;
+  return { plate, type, date, price };
 }
